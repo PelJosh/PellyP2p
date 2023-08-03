6c134546b9541bb822e66af10f4e5a7b2c9ae943
JoshuaOyed <joshua.oyedola@maxdrive.ai>
2023-08-03 03:19:34 +0100

    set up my typscript and nodejs environment for the project

---
+++ index.ts
@@ -0,0 +1,17 @@
+import express, {Express, Request, Response } from "express"
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT
+
+
+app.get ('/', (req, res) => {
+    res.send ('Express + Typescript server')
+});
+
+app.listen(port, () => {
+    console.log(`[server]: Server is running at http://localhost:${port}`);
+});
